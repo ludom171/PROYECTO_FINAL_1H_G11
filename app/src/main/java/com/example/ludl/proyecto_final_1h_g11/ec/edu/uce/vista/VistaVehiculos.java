114185f50d04f470c1a292c937b2969edb687e8f
Santiago <santiago.0905.saint@gmail.com>
2018-11-14 23:18:13 -0500

    commit

--- app/src/main/java/com/example/ludl/proyecto_final_1h_g11/ec/edu/uce/vista/VistaVehiculos.java
+++ app/src/main/java/com/example/ludl/proyecto_final_1h_g11/ec/edu/uce/vista/VistaVehiculos.java
@@ -4,19 +4,51 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.ludl.proyecto_final_1h_g11.R;
+import com.example.ludl.proyecto_final_1h_g11.ec.edu.uce.controlador.VehiculosControlador;
+import com.example.ludl.proyecto_final_1h_g11.ec.edu.uce.modelo.GlobalApplication;
+import com.example.ludl.proyecto_final_1h_g11.ec.edu.uce.modelo.Vehiculo;
+
+import java.io.File;
+import java.util.List;
 
 public class VistaVehiculos extends AppCompatActivity {
 
-    String lineatxt;
-    String txtcompleto;
+    ListView listaVehiculos;
+    String[] datosVehiculos;
+
+    VehiculosControlador  controlador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.vehiculos);
 
+        listaVehiculos=(ListView)findViewById(R.id.listaVehiculos);
+
+        try{
+            Vehiculo auxVehiculo= new Vehiculo();
+            this.getVehiculoControlador();
+
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+
+        Toast.makeText(this, "hola",Toast.LENGTH_LONG).show();
+
+        ArrayAdapter<String>adapter=new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1,datosVehiculos);
+        listaVehiculos.setAdapter(adapter);
+
+    }
+
+
+    public VehiculosControlador getVehiculoControlador(){
+
+        return new VehiculosControlador();
     }
 
 
