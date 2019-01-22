dfb32164c8886202f0e56625f9754505ba84e3a7
ludom171 <44213909+ludom171@users.noreply.github.com>
2019-01-21 19:46:41 -0500

    captura foto

--- app/src/main/java/com/example/ludl/proyecto_final_1h_g11/ec/edu/uce/vista/VistaReserva.java
+++ app/src/main/java/com/example/ludl/proyecto_final_1h_g11/ec/edu/uce/vista/VistaReserva.java
@@ -11,6 +11,7 @@ import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.ludl.proyecto_final_1h_g11.R;
 import com.example.ludl.proyecto_final_1h_g11.ec.edu.uce.modelo.Reserva;
@@ -18,7 +19,7 @@ import com.example.ludl.proyecto_final_1h_g11.ec.edu.uce.modelo.Reserva;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.util.Date;
-
+import java.util.regex.Pattern;
 
 
 public class VistaReserva extends AppCompatActivity {
@@ -44,7 +45,9 @@ public class VistaReserva extends AppCompatActivity {
 
     EditText valor_reserva;
 
-    Reserva reserva;
+    Boolean val_placa = false;
+
+    Reserva auxreserva;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,21 +56,21 @@ public class VistaReserva extends AppCompatActivity {
 
         //Inicializar Variales y captura de datos
 
-        regresar=(Button)findViewById(R.id.btn_regresar1);
-        reservar=(Button)findViewById(R.id.btn_reservar);
+        regresar = (Button) findViewById(R.id.btn_regresar1);
+        reservar = (Button) findViewById(R.id.btn_reservar);
 
-        num_reserva=(EditText) findViewById(R.id.txt_numero_reserva);
-        placa=(EditText) findViewById(R.id.txt_placa_reserva);
-        correo=(EditText) findViewById(R.id.txt_email_reserva);
-        celular=(EditText) findViewById(R.id.txt_celular);
+        num_reserva = (EditText) findViewById(R.id.txt_numero_reserva);
+        placa = (EditText) findViewById(R.id.txt_placa_reserva);
+        correo = (EditText) findViewById(R.id.txt_email_reserva);
+        celular = (EditText) findViewById(R.id.txt_celular);
 
-        selecfecha_reserva=(CalendarView) findViewById(R.id.calendario_prestamo);
-        auxFecha_reserva=(TextView) findViewById(R.id.text_fecha_prestamo);
+        selecfecha_reserva = (CalendarView) findViewById(R.id.calendario_prestamo);
+        auxFecha_reserva = (TextView) findViewById(R.id.text_fecha_prestamo);
 
-        selecfecha_entrega=(CalendarView) findViewById(R.id.calendario_entrega);
-        auxFecha_entrega=(TextView) findViewById(R.id.text_fecha_entrega);
+        selecfecha_entrega = (CalendarView) findViewById(R.id.calendario_entrega);
+        auxFecha_entrega = (TextView) findViewById(R.id.text_fecha_entrega);
 
-        valor_reserva=(EditText)findViewById(R.id.txt_valor_reserva);
+        valor_reserva = (EditText) findViewById(R.id.txt_valor_reserva);
 
 
         //Captura y conversion de Fecha de reserva
@@ -75,12 +78,12 @@ public class VistaReserva extends AppCompatActivity {
             @RequiresApi(api = Build.VERSION_CODES.N)
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int anio, int mes, int dia) {
-                mes=mes+1;
-                date_reserva = anio+"/"+mes+"/"+dia;
+                mes = mes + 1;
+                date_reserva = anio + "/" + mes + "/" + dia;
                 DateFormat formato;
-                formato= new java.text.SimpleDateFormat("yyyy/MM/dd");
+                formato = new java.text.SimpleDateFormat("yyyy/MM/dd");
                 try {
-                    fechaDate_reserva=(Date)formato.parse(date_reserva);
+                    fechaDate_reserva = (Date) formato.parse(date_reserva);
                 } catch (ParseException e) {
                     e.printStackTrace();
                 }
@@ -93,12 +96,12 @@ public class VistaReserva extends AppCompatActivity {
             @RequiresApi(api = Build.VERSION_CODES.N)
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int anio, int mes, int dia) {
-                mes=mes+1;
-                date_entrega = anio+"/"+mes+"/"+dia;
+                mes = mes + 1;
+                date_entrega = anio + "/" + mes + "/" + dia;
                 DateFormat formato;
-                formato= new java.text.SimpleDateFormat("yyyy/MM/dd");
+                formato = new java.text.SimpleDateFormat("yyyy/MM/dd");
                 try {
-                    fechaDate_entrega=(Date)formato.parse(date_entrega);
+                    fechaDate_entrega = (Date) formato.parse(date_entrega);
                 } catch (ParseException e) {
                     e.printStackTrace();
                 }
@@ -106,27 +109,81 @@ public class VistaReserva extends AppCompatActivity {
             }
         });
 
+        Intent i = getIntent();
 
-
-
-
-
-
-
-
+        //////ojojojojojojojo
+        //auxreserva = (Reserva) i.getSerializableExtra("reserva");
+        //if (auxreserva != null) cargarReserva(auxreserva);
 
 
         //Evento Regresar
         regresar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent newform = new Intent(VistaReserva.this,VistaMenu.class);
+                Intent newform = new Intent(VistaReserva.this, VistaMenu.class);
                 finish();
                 startActivity(newform);
             }
         });
 
 
+    }
+
+    public void saveReserva(View view) {
+        Pattern plc = Pattern.compile("^([A-Z]{3}-[0-9]{3,4})$");
+        if (plc.matcher(placa.getText().toString()).matches() == false) {
+            placa.setError("Placa Incorrecta");
+            Toast.makeText(this, "Formato Incorrecto placa", Toast.LENGTH_SHORT).show();
+            val_placa = false;
+        } else {
+            placa.setError(null);
+            val_placa = true;
+            try {
+
+                if (auxreserva == null) {
+                    auxreserva = new Reserva();
+                    auxreserva.setNum_reserva(null);
+                    Toast.makeText(this, "No hay reservas", Toast.LENGTH_SHORT).show();
+                }
+
+                //    auxVehiculo.setVehiculo(vehiculo.getText().toString());
+                auxreserva.setPlaca(placa.getText().toString());
+                auxreserva.setNum_reserva(num_reserva.getText().toString());
+
+                this.mensaje("Reserva Guardada");
+
+
+                placa.setText("");
+
+                correo.setText("");
+
+                celular.setText("");
+
+
+                valor_reserva.setText("");
+
+
+                Intent newform = new Intent(VistaReserva.this, VistaVehiculos.class);
+                finish();
+                startActivity(newform);
+
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                this.mensaje("Datos No Guardados");
+            }
+            Toast.makeText(this, "Formato Correcto", Toast.LENGTH_SHORT).show();
+        }
 
     }
+
+    public void mensaje(String texto) {
+        Toast.makeText(this, texto, Toast.LENGTH_LONG).show();
+    }
+
+    public void Aceptar(View view) {
+        Intent newform = new Intent(VistaReserva.this, VistaVehiculos.class);
+        finish();
+        startActivity(newform);
+    }
+
 }
